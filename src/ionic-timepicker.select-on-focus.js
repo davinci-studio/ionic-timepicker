77d78bdbfc7fd0bc74d3bbe3c793e13b076234a5
Jakub <jbinda@davinci-studio.eu>
2016-06-01 09:11:55 +0200

    Auto select for hours and minutes

---
+++ src/ionic-timepicker.select-on-focus.js
@@ -0,0 +1,61 @@
+//By Rajeshwar Patlolla
+//https://github.com/rajeshwarpatlolla
+
+(function () {
+  'use strict';
+
+  angular.module('ionic-timepicker')
+    .directive('selectOnFocus', selectOnFocus);
+
+  ionicTimepicker.$inject = ['$timeout'];
+  function selectOnFocus($timeout) {
+      return {
+          	restrict: 'A',
+            scope: {
+            	nextField: '@?'
+            },
+            require: 'ngModel',
+            link: function(scope, element, attributes, controller) {
+      				var focused = false;
+              var SWITCH_EVENT = 'FIELD_SWITCHED';
+              var maxLength = Number(attributes.maxlength) || 0;
+
+              function elementSelectAll(elem) {
+                  elem.setSelectionRange(0, elem.value.length);
+              }
+            	function focusHandler() {
+              	focused = true;
+              	$timeout(elementSelectAll, 0, false, this);
+              }
+              function blurHandler() {
+              	focused = false;
+                while (maxLength && getValue().length < maxLength) {
+                	controller.$setViewValue('0' + controller.$viewValue);
+                  controller.$render();
+                }
+              }
+              function getValue() {
+              	return controller.$viewValue;
+              }
+
+            	element.on('focus', focusHandler);
+            	element.on('blur', blurHandler);
+
+      				scope.$on(SWITCH_EVENT, function (event, fieldId) {
+              	if (fieldId === attributes.id) {
+                	element[0].focus();
+                }
+              });
+
+              if (scope.nextField && maxLength) {
+              	scope.$watch(getValue, function(newValue) {
+                	if (focused && newValue.length >= maxLength) {
+                  	scope.$parent.$broadcast(SWITCH_EVENT, scope.nextField);
+                  }
+                });
+              }
+            }
+        };
+  }
+
+})();
